cf687afbfb4155980c132c93cb1574cc650acb3a
王善能 <wangsn.tech@outlook.com>
2025-03-13 16:45:18 +0800

    feat：增加 unocss 配置

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import 'uno.css'; // 这里引入 UnoCSS
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
